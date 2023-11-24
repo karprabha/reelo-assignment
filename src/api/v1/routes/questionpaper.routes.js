305e4f39d736082ace49ed8c8dd3c4dd0b9a259c
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-11-25 05:28:33 +0530

    add question-papers post endpoint

--- src/api/v1/routes/questionpaper.routes.js
+++ src/api/v1/routes/questionpaper.routes.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
 
+import { questionPaperValidator } from "../validators/index.js";
+import { questionpaperController } from "../controllers/index.js";
+import { queryValidationMiddleware } from "../middlewares/index.js";
+
 const router = Router();
 
-router.get("/question-papers", (req, res, next) => res.json({ msg: "question-papers route" }));
+router.post(
+    "/question-papers",
+    questionPaperValidator.generateQuestionPaperValidator,
+    queryValidationMiddleware,
+    questionpaperController.generateQuestionPaper,
+);
 
 export default router;
